71299c24ef93c49b419320f2569645b41e65d137
Martyna Kuciara <mkuciara@o2.pl>
2021-05-04 13:23:24 +0200

    let changed to const

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
-let form = document.querySelector(".js-form");
-let amount = document.querySelector(".js-amount");
-let result = document.querySelector(".js-result");
-let submitElement = document.querySelector(".js-submit");
+{
+    const form = document.querySelector(".js-form");
+    const amount = document.querySelector(".js-amount");
+    const result = document.querySelector(".js-result");
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-    let eur = 4.56;
-    let pln = +amount.value;
+        const eur = 4.56;
+        const pln = +amount.value;
 
-    let finalResult = pln / eur;
+        const finalResult = pln / eur;
 
-    console.log(finalResult);
-    result.innerText = finalResult.toFixed(2);
+        console.log(finalResult);
+        result.innerText = finalResult.toFixed(2);
 
-});
+    });
+}
